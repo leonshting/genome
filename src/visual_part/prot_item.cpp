3ae6f71f059d92f92a9568716d4ec4c7e091e22f
BreeZa <breezin@ya.ru>
2014-05-22 00:31:42 -0700

    Update prot_item.cpp

--- src/visual_part/prot_item.cpp
+++ src/visual_part/prot_item.cpp
@@ -1,6 +1,6 @@
 #include "prot_item.h"
 
-prot_item::prot_item(int _x1, int _x2, int _width, QString _name,QBrush _brush, QPen _pen, int _level, QGraphicsItem *parent) :
+prot_item::prot_item(int _x1, int _x2, int _width, QString _name,QBrush _brush, QPen _pen, int _level, QGraphicsItem *parent) :/*describes objects for drawing*/
     QGraphicsItem(parent)
 {
     brush = _brush;
@@ -14,7 +14,7 @@ prot_item::prot_item(int _x1, int _x2, int _width, QString _name,QBrush _brush, 
     setToolTip(name+'\n'+ s1.sprintf("%d",x1)+ ".." + s2.sprintf("%d",x2));
 }
 
-void prot_item::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+void prot_item::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) /*describes function for drawing*/
 {
     QRectF rec = boundingRect();
     painter->setBrush(brush);
@@ -22,7 +22,7 @@ void prot_item::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
     painter->drawRect(rec);
 }
 
-QRectF prot_item::boundingRect() const
+QRectF prot_item::boundingRect() const  /*colour bound of window*/
 {
     return QRectF(x1,55+level*(width+5),x2-x1,width);
 }
